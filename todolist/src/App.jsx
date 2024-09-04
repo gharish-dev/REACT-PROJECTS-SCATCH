fd2b7db769749bbd0d4198cd6f22bf0f922c9d7a
harish galiboina <harish.joyy@gmail.com>
2024-09-04 22:57:14 +0530

    first commit

---
+++ todolist/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+    <div className="container">
+
+      <h1 className='text-center'> 
+        To DO APPLICATION 
+      </h1>
+    </div>
+    
+    
+    
+    </>
+  );
+}
+
+export default App;
